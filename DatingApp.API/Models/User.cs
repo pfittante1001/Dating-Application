11929e69fd97f3f0d69409d1b204ea9c4c1c6b81
pfittante1001 <pfittante@yahoo.com>
2018-10-11 21:43:57 -0400

    added user model Performed migration and database update

---
+++ DatingApp.API/Models/User.cs
@@ -0,0 +1,16 @@
+namespace DatingApp.API.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+
+        public string Username { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PassworSalt { get; set; }
+    
+                
+    
+    }
+}
