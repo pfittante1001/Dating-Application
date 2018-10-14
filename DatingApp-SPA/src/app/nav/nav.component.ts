86d3d584322780810463f6405000799ff0749437
pfittante1001 <pfittante@yahoo.com>
2018-10-14 11:11:48 -0400

    Connected Angular frontend login to api

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  model: any = {};
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
+
+  login() {
+  this.authService.login(this.model).subscribe(next => {
+    console.log('Logged in sucessfully');
+  }, error => {
+    console.log('Failed to login');
+  });
+
+}
 
 }
