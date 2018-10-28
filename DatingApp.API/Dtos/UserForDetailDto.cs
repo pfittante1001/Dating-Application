262efb047d9cd8b7d82dbfac46cbde8009b21677
pfittante1001 <pfittante@yahoo.com>
2018-10-28 15:43:05 -0400

    Complete automapping

--- DatingApp.API/Dtos/UserForDetailDto.cs
+++ DatingApp.API/Dtos/UserForDetailDto.cs
@@ -30,6 +30,6 @@ namespace DatingApp.API.Dtos
 
         public string PhotoUrl { get; set; }
 
-        public ICollection<Photo> Photos { get; set; }
+        public ICollection<PhotosForDetailDto> Photos { get; set; }
     }
 }
--- DatingApp.API/helpers/AutoMapperProfiles.cs
+++ DatingApp.API/helpers/AutoMapperProfiles.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
@@ -8,8 +9,21 @@ namespace DatingApp.API.helpers
     {
         public AutoMapperProfiles()
         {
-            CreateMap<User, UserForListDto>();
-            CreateMap<User, UserForDetailDto>();
+            CreateMap<User, UserForListDto>().ForMember(dest => dest.PhotoUrl, opt => {
+                opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+            })
+            .ForMember(dest => dest.Age, opt => {
+                opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+            });
+
+            CreateMap<User, UserForDetailDto>()
+            .ForMember(dest => dest.PhotoUrl, opt => {
+                opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+            })
+            .ForMember(dest => dest.Age, opt => {
+                opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+            });
+            CreateMap<Photo, PhotosForDetailDto>();
 
         }
     }
